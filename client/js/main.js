ec8e44eac381c648bf44879ab97409b3a40e8b3d
Eli Elliotson <elias.elliotson@gmail.com>
2024-10-01 13:50:33 -0400

    fix: create "wait for opponant" modal

--- client/js/main.js
+++ client/js/main.js
@@ -1,7 +1,7 @@
 import * as PIXI from "https://cdn.jsdelivr.net/npm/pixi.js@7.4.2/+esm";
 import { TiledBackground } from "./pixi/common/TiledBackground.js";
 import { LoopedAudio } from "./audio/LoopedAudio.js";
-import { GameSettingsModal } from "./pixi/units/GameSettingsModal.js";
+import { WaitForOpponantModal } from './pixi/units/WaitForOpponantModal.js';
 
 PIXI.BaseTexture.defaultOptions.scaleMode = 0;
 
@@ -11,7 +11,7 @@ document.body.appendChild(app.view);
 const bg = new TiledBackground("./assets/nes/img/bg/menu.png");
 app.stage.addChild(bg);
 
-const gameSettingsModal = new GameSettingsModal();
+const gameSettingsModal = new WaitForOpponantModal();
 app.stage.addChild(gameSettingsModal);
 
 let menuBgm = new LoopedAudio("./assets/nes/music/menu.mp3", 22088 / 44100, 867413 / 44100);
---
+++ client/js/pixi/units/WaitForOpponantModal.js
@@ -0,0 +1,20 @@
+import * as PIXI from "https://cdn.jsdelivr.net/npm/pixi.js@7.4.2/+esm";
+import { Modal } from "../common/Modal.js";
+
+export class WaitForOpponantModal extends Modal {
+  opponentText
+
+  constructor() {
+    super();
+
+    // Add display text
+    this.opponentText = PIXI.Sprite.from("./assets/nes/img/gui/waiting-for-opponent.png")
+    this.addChild(this.opponentText);
+
+    // Add positioning event listener
+    this.opponentText.texture.baseTexture.on("loaded", () => {
+      this.opponentText.position.x = ((this.width / this.scale.x) - this.opponentText.width) / 2;
+      this.opponentText.position.y = ((this.height / this.scale.y) - this.opponentText.height) / 2;
+    })
+  }
+}
